eba4b31c2c0c7d798b9993530ff9896e44a301a5
Dnyaneshwar Mane <60722778+DannyDMane@users.noreply.github.com>
2023-05-24 12:16:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-import pandas
+
 streamlit.title('My Parents new Healthy Diner')
 
 streamlit.header('🥣 Breakfast Menu')
@@ -10,11 +10,15 @@ streamlit.text('🥑 🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
@@ -23,7 +27,7 @@ streamlit.dataframe(my_fruit_list)
 
 # streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado'],['Strawberries'])
 Fruits_Selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado'],['Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[Fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
